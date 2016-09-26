cf97820edc1af2646751bc364e04ecaa6b416b91
matthewfish <danielfishbein1@Daniels-MacBook-Pro.local>
2016-09-26 11:55:13 -0700

    commit

--- Function Fun.py
+++ Function Fun.py
@@ -44,3 +44,13 @@ def convertFromSeconds( s ):
     return [days, hours, minutes, seconds]
 print convertFromSeconds (200000)
 
+def front3(str):
+    """
+    Given a string, we'll say that the
+    front is the first 3 chars of the string.
+    If the string length is less than 3,
+    the front is whatever is there. Return a new
+    string which is 3 copies of the front.
+    """
+    return str[:3]*3
+
