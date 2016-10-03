1c69a8c09b3370bbd34de9ca06299311e2513548
matthewfish <danielfishbein1@Daniels-MacBook-Pro.local>
2016-10-03 12:18:47 -0700

    committ

---
+++ Multiplication Game.py
@@ -0,0 +1,33 @@
+
+
+def instruction():
+    print "Hi, I am Eric. Were going to practice Multiplication!"
+
+def play():
+    import random
+    for i in range(5):
+        userAnswer = -1
+        factor1 = random.randint(0,15)
+        factor2 = random.randint(0,15)
+        correctAnswer = factor1 * factor2
+
+        while userAnswer != correctAnswer:
+            userAnswer = raw_input(" please enter the product of " + str(factor1) + " and " + str(factor2) + ":")
+            correctAnswer = factor1 * factor2
+            try:
+                userAnswer = int(userAnswer)
+                if userAnswer == correctAnswer:
+                    print "correct"
+                else:
+                    print "incorrect"
+            except:
+               print 'enter an integer'
+
+
+
+
+
+def main():
+   instruction()
+   play()
+main()
