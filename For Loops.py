feeee9beacb23ed2c5dc3b77f60ef619795ef918
matthewfish <danielfishbein1@Daniels-MacBook-Pro.local>
2016-10-05 08:21:13 -0700

    commit

--- For Loops.py
+++ For Loops.py
@@ -21,14 +21,61 @@ def fac(n):
 print "fac(0): should be 1 == ", fac(0)
 print "fac(5): should be 120 == ", fac(5)
 
-def power (b,P):
+def power(b,p):
     result = 1
-    for x in range (p):
+    for x in range(p):
         result = result*b
+        print result
+    return result
 
+
+'''
 print "power(2,5): should be 32 == ", power(2,5)
 print "power(5,2): should be 25 == ", power(5,2)
 print "power(42,0): should be 1 == ", power(42,0)
 print "power(0,42): should be 0 == ", power(0,42)
 print "power(0,0): should be 1 == ", power(0,0)
+'''
 
+def mult(m,n):
+    result = 0
+    if n>= 0:
+        for x in range(n):
+            result = result + m
+            #print result
+        return result
+    else:
+        for x in range(-1*n):
+            result = result + m
+            #print result
+        return result*-1
+
+
+print "mult(6,7)    42 ==", mult(6,7)
+print "mult(6,-7)  -42 ==", mult(6,-7)
+print "mult(-6,7)  -42 ==", mult(-6,7)
+print "mult(-6,-7)  42 ==", mult(-6,-7)
+print "mult(6,0)     0 ==", mult(6,0)
+print "mult(0,7)     0 ==", mult(0,7)
+print "mult(0,0)     0 ==", mult(0,0)
+
+
+print mult(6,7)
+
+
+def count_evens():
+    result =
+    if
+
+
+
+print "count_evens([2, 1, 2, 3, 4], 3 == ", count_evens([2, 1, 2, 3, 4])
+print "count_evens([2, 2, 0]), 3 == ", count_evens([2, 2, 0])
+print "count_evens([1, 3, 5]), 0 == ", count_evens([1, 3, 5])
+
+
+
+
+print "count9([1, 2, 9]), 1 == ",count9([1, 2, 9])
+print "count9([1, 9, 9]), 2 == ",count9([1, 9, 9])
+print "count9([1, 9, 9, 3, 9]), 3 == ",count9([1, 9, 9, 3, 9])
