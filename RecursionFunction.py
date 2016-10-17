a382519689ca50cd51e539908e623ff9e11137f5
matthewfish <danielfishbein1@Daniels-MacBook-Pro.local>
2016-10-17 13:47:44 -0700

    commit changes

--- RecursionFunction.py
+++ RecursionFunction.py
@@ -1,10 +1,21 @@
-listReverseIter(l)
 
 def fib(n):
     if n == 0:
-        return 0
-    if n == 1:
         return 1
-
+    if n ==1:
+        return 1
     if n > 1:
-        fib()
+        return fib(n-2)+fib(n-1)
+
+
+def fibter(n):
+    fibseq = [0,1]
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        for i in range(2,n):
+            fibseq.append(fibseq[i-1]+fibseq[i-2])
+            print fibseq[-1]
+        return fibseq[-1]
